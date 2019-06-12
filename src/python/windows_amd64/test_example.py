7c0529ff7d619d37c54c5b66a6099fe93958abc0
Pedro Lopes <plopesresearch@gmail.com>
2019-06-11 20:57:45 -0500

    deleted py windows example, was repeated

--- src/python/windows_amd64/test_example.py
+++
@@ -1,9 +0,0 @@
-from rehamove import *
-import time
-
-# Trying on Windows, connecting to USB port COM3
-r = Rehamove("COM3")
-r.battery()
-for i in range(0, 5):
-	r.pulse("blue", 6, 200)
-	time.sleep(1)
