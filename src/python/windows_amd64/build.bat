c0b658ba5bade583ae578285c7a98f098db0249e
Aaron Tang <awtang@uchicago.edu>
2019-06-12 20:40:49 -0500

    cleaned build script (for Python on Windows) even further using variables to set directory names

--- src/python/windows_amd64/build.bat
+++ src/python/windows_amd64/build.bat
@@ -1,13 +1,16 @@
-echo Performing the build
+SET build_directory=../../../builds/python/windows_amd64
+SET precompiled_directory=../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static
+
+echo rehamove-integration-lib: Performing the build
 swig -python rehamovelib.i
-cl /LD /MD rehamovelib.c rehamovelib_wrap.c /Fe_rehamovelib.pyd /I "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/include" /I ../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/low-level /I ../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/general /link ../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/lib/libsmpt.lib "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/libs/python37.lib"
+cl /LD /MD rehamovelib.c rehamovelib_wrap.c /Fe_rehamovelib.pyd /I "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/include" /I "%precompiled_directory%/include/low-level" /I "%precompiled_directory%/include/general" /link "%precompiled_directory%/lib/libsmpt.lib" "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/libs/python37.lib"
 
-echo Moving built files into the builds directory
-copy "rehamove.py" "../../../builds/python/windows_amd64/"
-move "rehamovelib.py" "../../../builds/python/windows_amd64/"
-move "_rehamovelib.pyd" "../../../builds/python/windows_amd64/"
+echo rehamove-integration-lib: Moving built files into the builds directory
+copy rehamove.py "%build_directory%"
+move rehamovelib.py "%build_directory%"
+move _rehamovelib.pyd "%build_directory%"
 
-echo Cleaning up intermediate build files
+echo rehamove-integration-lib: Cleaning up intermediate build files
 del rehamovelib_wrap.c
 del rehamovelib.obj
 del rehamovelib_wrap.obj
