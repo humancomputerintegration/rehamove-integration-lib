e38435eb6d92d23318ae813459874b8c3e1212e4
Aaron Tang <awtang@uchicago.edu>
2019-06-12 20:30:49 -0500

    updated Python Windows build to move built files to builds directory, and to clean up intermediate build files

--- src/python/windows_amd64/build.bat
+++ src/python/windows_amd64/build.bat
@@ -1,2 +1,15 @@
+echo Performing the build
 swig -python rehamovelib.i
-cl /LD /MD rehamovelib.c rehamovelib_wrap.c /Fe_rehamovelib.pyd /I "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/include" /I "C:/Users/Agggron\Documents/UCHICAGO/SPRING_2019/rehamove-library/rehamove-library/hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/low-level" /I "C:/Users/Agggron\Documents/UCHICAGO/SPRING_2019/rehamove-library/rehamove-library/hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/general" /link "C:/Users/Agggron\Documents/UCHICAGO/SPRING_2019/rehamove-library/rehamove-library/hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/lib/libsmpt.lib" "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/libs/python37.lib"
+cl /LD /MD rehamovelib.c rehamovelib_wrap.c /Fe_rehamovelib.pyd /I "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/include" /I ../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/low-level /I ../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/general /link ../../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/lib/libsmpt.lib "C:/Users/Agggron/AppData/Local/Programs/Python/Python37/libs/python37.lib"
+
+echo Moving built files into the builds directory
+copy "rehamove.py" "../../../builds/python/windows_amd64/"
+move "rehamovelib.py" "../../../builds/python/windows_amd64/"
+move "_rehamovelib.pyd" "../../../builds/python/windows_amd64/"
+
+echo Cleaning up intermediate build files
+del rehamovelib_wrap.c
+del rehamovelib.obj
+del rehamovelib_wrap.obj
+del _rehamovelib.lib
+del _rehamovelib.exp
