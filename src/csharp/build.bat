fbb6a249bffd32a2f131c8db671b6b6d55f410cb
Aaron Tang <awtang@uchicago.edu>
2019-06-12 20:57:34 -0500

    updated build script for C# on Windows, and cleaned src and build directories for this language/target

--- src/csharp/build.bat
+++ src/csharp/build.bat
@@ -1,5 +1,21 @@
+SET build_directory=../../builds/csharp/
+SET precompiled_directory=../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static
+
+echo rehamove-integration-lib: Performing the build (C# on Windows)
 swig -csharp rehamovelib.i
-cl /LD /MD rehamovelib.c rehamovelib_wrap.c /Ferehamovelib.dll /I ../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/low-level/ /I ../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/include/general /link ../../hasomed_precompiled/smpt_rm3_msvc2015_x86_amd64_static/lib/libsmpt.lib
+cl /LD /MD rehamovelib.c rehamovelib_wrap.c /Ferehamovelib.dll /I "%precompiled_directory%/include/low-level/" /I "%precompiled_directory%/include/general" /link "%precompiled_directory%/lib/libsmpt.lib"
+
+echo rehamove-integration-lib: Moving built files into the builds directory
+move rehamovelib.dll "%build_directory%"
+
+echo rehamove-integration-lib: Cleaning up intermediate build files
+del rehamovelib_wrap.c
+del rehamovelib.obj
+del rehamovelib_wrap.obj
+del rehamovelib.lib
+del rehamovelib.exp
+
+echo rehamove-integration-lib: Adding in namespace lines for files used in UnityRehamove.dll
 
 echo }>backspace.cs
 
